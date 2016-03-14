33b4b8263e2a9c72ce48d838dc6d52e62bf73097
Rob Halff <rob.halff@gmail.com>
2016-03-14 03:10:51 +0100

    use object.keys

--- 10.js
+++ 10.js
@@ -1,84 +1,114 @@
-module.exports = function forIns (fn, obj, argLength) {
+module.exports = function objk (fn, o0, argLength) {
   var res = []
-  for (var k0 in obj) {
-    if (obj.hasOwnProperty(k0)) {
-      if (argLength > 1) {
-        for (var k1 in obj[k0]) {
-          if (obj[k0].hasOwnProperty(k1)) {
-            if (argLength > 2) {
-              for (var k2 in obj[k0][k1]) {
-                if (obj[k0][k1].hasOwnProperty(k2)) {
-                  if (argLength > 3) {
-                    for (var k3 in obj[k0][k1][k2]) {
-                      if (obj[k0][k1][k2].hasOwnProperty(k3)) {
-                        if (argLength > 4) {
-                          for (var k4 in obj[k0][k1][k2][k3]) {
-                            if (obj[k0][k1][k2][k3].hasOwnProperty(k4)) {
-                              if (argLength > 5) {
-                                for (var k5 in obj[k0][k1][k2][k3][k4]) {
-                                  if (obj[k0][k1][k2][k3][k4].hasOwnProperty(k5)) {
-                                    if (argLength > 6) {
-                                      for (var k6 in obj[k0][k1][k2][k3][k4][k5]) {
-                                        if (obj[k0][k1][k2][k3][k4][k5].hasOwnProperty(k6)) {
-                                          if (argLength > 7) {
-                                            for (var k7 in obj[k0][k1][k2][k3][k4][k5][k6]) {
-                                              if (obj[k0][k1][k2][k3][k4][k5][k6].hasOwnProperty(k7)) {
-                                                if (argLength > 8) {
-                                                  for (var k8 in obj[k0][k1][k2][k3][k4][k5][k6][k7]) {
-                                                    if (obj[k0][k1][k2][k3][k4][k5][k6][k7].hasOwnProperty(k8)) {
-                                                      if (argLength > 9) {
-                                                        for (var k9 in obj[k0][k1][k2][k3][k4][k5][k6][k7][k8]) {
-                                                          if (obj[k0][k1][k2][k3][k4][k5][k6][k7][k8].hasOwnProperty(k9)) {
-                                                            if (argLength > 10) {
-   throw Error('more than 10 arguments not supported')
-                                                            } else { var ret = fn(k0, k1, k2, k3, k4, k5, k6, k7, k8, k9,
-                                                                obj[k0][k1][k2][k3][k4][k5][k6][k7][k8][k9])
-                                                              if (ret !== undefined) res.push(ret) }
-                                                          }
-                                                        }
-                                                      } else { var ret = fn(k0, k1, k2, k3, k4, k5, k6, k7, k8,
-                                                          obj[k0][k1][k2][k3][k4][k5][k6][k7][k8])
-                                                        if (ret !== undefined) res.push(ret) }
-                                                    }
-                                                  }
-                                                } else { var ret = fn(k0, k1, k2, k3, k4, k5, k6, k7,
-                                                    obj[k0][k1][k2][k3][k4][k5][k6][k7])
-                                                  if (ret !== undefined) res.push(ret) }
-                                              }
-                                            }
-                                          } else { var ret = fn(k0, k1, k2, k3, k4, k5, k6,
-                                              obj[k0][k1][k2][k3][k4][k5][k6])
-                                            if (ret !== undefined) res.push(ret) }
+  var k0 = Object.keys(o0)
+  for (var a = 0; a < k0.length; a++) {
+    var o1 = o0[k0[a]]
+    if (argLength > 1) {
+      var k1 = Object.keys(o1)
+      for (var b = 0; b < k1.length; b++) {
+        var o2 = o1[k1[b]]
+        if (argLength > 2) {
+          var k2 = Object.keys(o2)
+          for (var c = 0; c < k2.length; c++) {
+            var o3 = o2[k2[c]]
+            if (argLength > 3) {
+              var k3 = Object.keys(o3)
+              for (var d = 0; d < k3.length; d++) {
+                var o4 = o3[k3[d]]
+                if (argLength > 4) {
+                  var k4 = Object.keys(o4)
+                  for (var e = 0; e < k4.length; e++) {
+                    var o5 = o4[k4[e]]
+                    if (argLength > 5) {
+                      var k5 = Object.keys(o5)
+                      for (var f = 0; f < k5.length; f++) {
+                        var o6 = o5[k5[f]]
+                        if (argLength > 6) {
+                          var k6 = Object.keys(o6)
+                          for (var g = 0; g < k6.length; g++) {
+                            var o7 = o6[k6[g]]
+                            if (argLength > 7) {
+                              var k7 = Object.keys(o7)
+                              for (var h = 0; h < k7.length; h++) {
+                                var o8 = o7[k7[h]]
+                                if (argLength > 8) {
+                                  var k8 = Object.keys(o8)
+                                  for (var i = 0; i < k8.length; i++) {
+                                    var o9 = o8[k8[i]]
+                                    if (argLength > 9) {
+                                      var k9 = Object.keys(o9)
+                                      for (var j = 0; j < k9.length; j++) {
+                                        var o10 = o9[k9[j]]
+                                        if (argLength > 10) {
+  throw Error('more than 10 arguments not supported')
+                                        } else {
+                                          var ret = fn (k0[a], k1[b], k2[c], k3[d], k4[e], k5[f], k6[g], k7[h], k8[i], k9[j], o10)
+                                          if (ret !== undefined) {
+                                            res.push(ret)
+                                          }
                                         }
                                       }
-                                    } else { var ret = fn(k0, k1, k2, k3, k4, k5,
-                                        obj[k0][k1][k2][k3][k4][k5])
-                                      if (ret !== undefined) res.push(ret) }
+                                    } else {
+                                      var ret = fn (k0[a], k1[b], k2[c], k3[d], k4[e], k5[f], k6[g], k7[h], k8[i], o9)
+                                      if (ret !== undefined) {
+                                        res.push(ret)
+                                      }
+                                    }
+                                  }
+                                } else {
+                                  var ret = fn (k0[a], k1[b], k2[c], k3[d], k4[e], k5[f], k6[g], k7[h], o8)
+                                  if (ret !== undefined) {
+                                    res.push(ret)
                                   }
                                 }
-                              } else { var ret = fn(k0, k1, k2, k3, k4,
-                                  obj[k0][k1][k2][k3][k4])
-                                if (ret !== undefined) res.push(ret) }
+                              }
+                            } else {
+                              var ret = fn (k0[a], k1[b], k2[c], k3[d], k4[e], k5[f], k6[g], o7)
+                              if (ret !== undefined) {
+                                res.push(ret)
+                              }
                             }
                           }
-                        } else { var ret = fn(k0, k1, k2, k3,
-                            obj[k0][k1][k2][k3])
-                          if (ret !== undefined) res.push(ret) }
+                        } else {
+                          var ret = fn (k0[a], k1[b], k2[c], k3[d], k4[e], k5[f], o6)
+                          if (ret !== undefined) {
+                            res.push(ret)
+                          }
+                        }
+                      }
+                    } else {
+                      var ret = fn (k0[a], k1[b], k2[c], k3[d], k4[e], o5)
+                      if (ret !== undefined) {
+                        res.push(ret)
                       }
                     }
-                  } else { var ret = fn(k0, k1, k2,
-                      obj[k0][k1][k2])
-                    if (ret !== undefined) res.push(ret) }
+                  }
+                } else {
+                  var ret = fn (k0[a], k1[b], k2[c], k3[d], o4)
+                  if (ret !== undefined) {
+                    res.push(ret)
+                  }
                 }
               }
-            } else { var ret = fn(k0, k1,
-                obj[k0][k1])
-              if (ret !== undefined) res.push(ret) }
+            } else {
+              var ret = fn (k0[a], k1[b], k2[c], o3)
+              if (ret !== undefined) {
+                res.push(ret)
+              }
+            }
+          }
+        } else {
+          var ret = fn (k0[a], k1[b], o2)
+          if (ret !== undefined) {
+            res.push(ret)
           }
         }
-      } else { var ret = fn(k0,
-          obj[k0])
-        if (ret !== undefined) res.push(ret) }
+      }
+    } else {
+      var ret = fn (k0[a], o1)
+      if (ret !== undefined) {
+        res.push(ret)
+      }
     }
   }
   return res
