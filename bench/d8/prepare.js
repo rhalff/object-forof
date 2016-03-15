04601bf3eae576e2fd59dab2deb6b4648a34bb9b
Rob Halff <rob.halff@gmail.com>
2016-03-16 00:06:51 +0100

    cleanup

--- bench/d8/prepare.js
+++ bench/d8/prepare.js
@@ -5,11 +5,11 @@ const path = require('path')
 const scripts = [
   'const data = ' + JSON.stringify(require('../data/bench_data.json')),
   "function isObject(obj) { return typeof obj === 'object'; }",
+  'const func = (key, key2, obj) => [key, key2, obj]',
   require('../../10.js').toString(),
   require('../../index').toString(),
   require('../util').object_keys.toString(),
   require('../util').for_in.toString(),
-  fs.readFileSync('./bench.js').toString(),
   'for (var i = 0; i < 1e4; i++) {',
   undefined,
   '}'
@@ -19,7 +19,7 @@ forOf((name, fn) => {
   scripts[8] = fn
   fs.writeFile(path.join('./bat/', name + '.js'), scripts.join('\n\n'))
 }, {
-  object_keys: 'bench_object_keys()',
-  for_in: 'bench_for_in()',
-  forOf: 'bench_forOf()'
+  object_keys: 'object_keys(func, data)',
+  for_in: 'for_in(func, data)',
+  forOf: 'forOf(func, data)'
 })
