055ab64e0dfeccea2f986ed66c31c9739ee0b816
Rob Halff <rob.halff@gmail.com>
2016-03-15 15:13:43 +0100

    lint

--- genk.js
+++ genk.js
@@ -29,10 +29,6 @@ module.exports = function objk(fn, o0, argLength) {
 
 var size = 10
 
-function keys (k) {
-  return k.length ? `[${k.join('][')}]` : ''
-}
-
 function gen (length) {
   var alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k']
   let head = [
@@ -42,21 +38,19 @@ function gen (length) {
   ]
 
   let tail = []
-  let d = []
   let ind1 = '  '
-  let ind2 = '  ' + ' '.repeat(length * 7)
   let kpath = []
   for (var i = 0; i < length; i++) {
     const k = `k${i}`
     const o = `o${i}`
-    const on = `o${i+1}`
+    const on = `o${i + 1}`
     const j = alpha[i]
     kpath.push(`${k}[${j}]`)
 
     head.push(`${ind1}var ${k} = Object.keys(${o})`)
     head.push(`${ind1}for (var ${j} = 0; ${j} < ${k}.length; ${j}++) {`)
     head.push(`${ind1}  var ${on} = ${o}[${k}[${j}]]`)
-    head.push(`${ind1}  if (argLength > ${i+1} && isObject(${on})) {`)
+    head.push(`${ind1}  if (argLength > ${i + 1} && isObject(${on})) {`)
 
     // reversed
     tail.push(`${ind1}}`)
